cff2116fa12bcb2a0fdc5267c5746cef68adb364
milly02online <milly02online@gmail.com>
2025-08-12 10:18:57 -0300

    Navegação básica entre telas

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
 import { ThemeProvider } from './src/contexts/ThemeContext';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <HomeScreen/>
+      {/* <HomeScreen/> */}
+      <RootNavigator/>
     </ThemeProvider>
     
   );
