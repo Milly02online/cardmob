0fec6cf498613eda52f61210f82dc860ebbbf1a5
milly02online <milly02online@gmail.com>
2025-12-02 10:28:54 -0300

    Uso básico de imagens

--- Etapa3/loja-react-app/src/screens/auth/ProfileScreen.tsx
+++ Etapa3/loja-react-app/src/screens/auth/ProfileScreen.tsx
@@ -1,73 +1,78 @@
-import React, { useEffect, useState } from "react"; // modificado
-import { View, Text, Button, StyleSheet, Image } from "react-native"; // modificado
+import React, { useEffect, useState } from 'react'; // modificado
+import { View, Text, Button, StyleSheet, Image } from 'react-native'; // modificado
 
-import { useTheme } from '../../contexts/ThemeContext'
+import { useTheme } from '../../contexts/ThemeContext';
 import { useAuth } from '../../contexts/AuthContext';
 
-import { requestProfileById } from "../../services/profileService"; // novo 
+import { requestProfileById } from '../../services/profileService'; // novo
 
 function ProfileScreen({ navigation }: any) {
     const { theme, toggleTheme } = useTheme();
     const { logout, userData } = useAuth();
-    const [user, setUser] = useState({}); // novo
+    const [user, setUser] = useState({});
 
     // novo
     useEffect(() => {
         const fetchProfile = async () => {
             try {
-                console.log(userData); //novo
-                const user = await requestProfileById(userData?.id); //correção
+                console.log(userData)
+                const user = await requestProfileById(userData?.id);
                 console.log(user);
                 setUser(user);
                 console.log('Carregou o usuário!');
-            }
-            catch (error) {
+            } catch (error) {
                 console.error('Erro ao carregar o perfil do usuário:', error);
             }
-        }
+        };
         fetchProfile();
     }, []);
 
     return (
-        <View style= {[ styles.container, {backgroundColor: theme.colors.background} ]}>
-            <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
+        <View
+            style={[
+                styles.container,
+                { backgroundColor: theme.colors.background },
+            ]}
+        >
+            <Text
+                style={{
+                    color: theme.colors.text,
+                    marginBottom: theme.spacing(1),
+                }}
+            >
                 Profile Screen
             </Text>
-
             <View>
-                <Image source={{ uri: user.image }} style={styles.image}/>
+                <Image source={{ uri: user.image }} style={styles.image} />
             </View>
-            
             <Text style={styles.text}>{user.name}</Text>
             <Text style={styles.text}>{user.email}</Text>
 
-            <Button 
-                title="Alternar Tema" 
-                color={theme.colors.primary} 
+            <Button
+                title="Alternar Tema"
+                color={theme.colors.primary}
                 onPress={toggleTheme}
             />
-            <Button 
+            <Button
                 title="Ir para Detalhes"
                 onPress={() => navigation.navigate('Details')}
             />
-            <Button 
-                title="Sair"
-                onPress={ logout }
-            />
+            <Button title="Sair" onPress={logout} />
         </View>
-    )
+    );
 }
 export default ProfileScreen;
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    image: { // novo
+    image: {
+        // novo
         height: 100,
         width: 100,
     },
-    text: { fontSize: 14} // novo
+    text: { fontSize: 14 }, // novo
 });
